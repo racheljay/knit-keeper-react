76e4f1ac9286d31f5c86c83417228258298d0004
Rachel <racheljay42@gmail.com>
2020-11-25 17:10:11 -0500

    basic create user working

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { axiosHelper } from './utilities/axiosHelper';
+import axios from 'axios';
 
 
 function Register() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const data = { name, email, password };
+    const headers = {
+        'Content_Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*'
+    }
+    const method = 'post';
+    const url = '/register';
+
+
     return (
         <div className="container">
             <h1>New Account:</h1>
@@ -10,26 +26,51 @@ function Register() {
             <div className="form-group row">
                 <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">User Name</label>
                 <div className="col-sm-10">
-                    <input type="username" className="form-control" id="inputEmail3" />
+                    <input
+                        type="username"
+                        className="form-control"
+                        id="inputEmail3"
+                        placeholder="username"
+                        onChange={e => setName(e.target.value)}
+                        value={name}
+                    />
                 </div>
             </div>
 
             <div className="form-group row">
                 <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
                 <div className="col-sm-10">
-                    <input type="email" className="form-control" id="inputEmail3" />
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="inputPassword3"
+                        placeholder="Email"
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
+                    />
                 </div>
             </div>
             <div className="form-group row">
                 <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                 <div className="col-sm-10">
-                    <input type="password" className="form-control" id="inputPassword3" />
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="inputPassword3"
+                        placeholder="Password"
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
+                    />
                 </div>
             </div>
 
             <div className="form-group row">
                 <div className="col-sm-10">
-                    <button type="submit" className="btn btn-danger">Create Account</button>
+                    <button
+                        type="submit"
+                        className="btn btn-danger"
+                        onClick={() => axiosHelper(method, data, url, headers)}
+                    >Create Account</button>
                 </div>
             </div>
             <Link to="/">Oh wait, I have an account</Link>
--- src/utilities/axiosHelper.jsx
+++ src/utilities/axiosHelper.jsx
@@ -1,3 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 
+export function axiosHelper(method, data, url, headers) {
+    const API_URL = 'http://localhost:8000'
+    return axios(
+        {
+            method,
+            url: API_URL + url,
+            data,
+        } 
+    ).then(res => {
+        console.log(res)
+    }).catch(e => console.log(e))
+}
+
+// export default axiosHelper;
